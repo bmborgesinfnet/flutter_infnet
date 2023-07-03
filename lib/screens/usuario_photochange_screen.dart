5a6c7db221c30b48ff72bca82964488c98f9104c
Bruno Borges <brunomborges@gmail.com>
2023-07-02 23:38:55 -0300

    02062023

--- lib/screens/usuario_photochange_screen.dart
+++ lib/screens/usuario_photochange_screen.dart
@@ -1,7 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_infnet/routes/RoutePaths.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import '../providers/usuario_provider.dart';
+import 'package:image_picker/image_picker.dart';
 
 class PhotoChangeScreen extends StatefulWidget {
   const PhotoChangeScreen({Key? key}) : super(key: key);
@@ -10,11 +14,43 @@ class PhotoChangeScreen extends StatefulWidget {
 }
 
 class _PhotoChangeScreenState extends State<PhotoChangeScreen> {
+  File? image;
   @override
   Widget build(BuildContext context) {
 
     final usuarioProvider = Provider.of<UsuarioProvider>(context);
     final usuarioSelecionado = usuarioProvider.getUsuarioSelecionado();
+
+    void alterarImagem() async {
+      final picker = ImagePicker();
+
+      final pickImage = await picker.pickImage(
+          source: ImageSource.gallery,
+          imageQuality: 50,
+          maxWidth: 200,
+      );
+
+      if (pickImage != null) {
+        setState(() {
+          image = File(pickImage.path);
+        });
+      }
+    }
+
+    void salvarImagem() {
+      final firebaseStorage = FirebaseStorage.instance;
+      final reference = firebaseStorage.ref("user/${usuarioSelecionado.id}.jpg");
+      final upload = reference.putFile(image!);
+      upload.whenComplete(() {
+        print("Upload realizado com sucesso.");
+        Navigator.of(context).pushNamedAndRemoveUntil(RoutePaths.USUARIO_LIST_SCREEN, (route) => false);
+      });
+      upload.catchError((error, stackTrace) {
+        print("Upload falhou: $error");
+      });
+    }
+
+    
 
     return Scaffold(
       appBar: AppBar(title: const Text("Informações Usuário")),
@@ -27,27 +63,37 @@ class _PhotoChangeScreenState extends State<PhotoChangeScreen> {
               SizedBox(
                 height: 400,
                 width: 400,
-                child: Image.network(
-                  "https://source.unsplash.com/random",
-                  loadingBuilder: (BuildContext context, Widget child,
-                    ImageChunkEvent? loadingProgress) {
-                    if (loadingProgress == null) {
-                      return child;
-                    }
-                    return Center(
-                      child: CircularProgressIndicator(
-                        value: loadingProgress.expectedTotalBytes != null
-                            ? loadingProgress.cumulativeBytesLoaded /
-                                loadingProgress.expectedTotalBytes!
-                            : null,
-                      ),
-                    );
-                  },
-                ),
+                child: 
+                  image == null?
+                  Image.network(
+                    "https://source.unsplash.com/random",
+                    loadingBuilder: (BuildContext context, Widget child,
+                      ImageChunkEvent? loadingProgress) {
+                      if (loadingProgress == null) {
+                        return child;
+                      }
+                      return Center(
+                        child: CircularProgressIndicator(
+                          value: loadingProgress.expectedTotalBytes != null
+                              ? loadingProgress.cumulativeBytesLoaded /
+                                  loadingProgress.expectedTotalBytes!
+                              : null,
+                        ),
+                      );
+                    },
+                  ) : 
+                  Image.file(image!),
               ),
               Text("Nome ${usuarioSelecionado.name}"),
               Text("Email ${usuarioSelecionado.email}"),
               Text("Telefone ${usuarioSelecionado.phone}"),
+              Center(child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                ElevatedButton(onPressed: () => alterarImagem(), child: const Text("Alterar")),
+              ],)
+              )
             ],
           ),
         ),
